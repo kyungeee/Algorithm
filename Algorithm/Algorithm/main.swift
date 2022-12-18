354c34fa37db6fe1e55b7340c301de5aff5149c0
Wonder Park <pkkyung26@gmail.com>
2022-12-19 00:30:04 +0900

    [Add] Programmers file

--- Algorithm/Algorithm/main.swift
+++ Algorithm/Algorithm/main.swift
@@ -6,56 +6,60 @@
 //
 
 
-let n = Int(readLine()!)! // 27
-var arr: [[String]] = Array(repeating: Array(repeating: " ", count: n), count: n)
+import Foundation
 
-// 9
-func recur(xstart: Int, ystart: Int, xend: Int, yend: Int) {
+var input = readLine()!.map { String($0) }
+
+
+var temp: [String] = []
+var index: Int = 0
+
+var minvalue = 10000000
+
+func compression(count: Int) -> Int {
+    var arr: [String] = []
+    var cut = 0
+    var comcount: Int = 1
+    var output: String = ""
     
-    // 2
-    let sum = (xend - xstart) / 3
+    // temp = [a, b, c]
+    for i in 0..<count {
+        temp.append(input[i])
+    }
     
-    if sum == 0 {
-        for i in xstart...xend {
-            for j in ystart...yend {
-                if (i == xstart + 1 && j == ystart + 1) {
-                    arr[j][i] = " "
-                }else {
-                    arr[j][i] = "*"
+    for i in count-1..<input.count {
+        cut += 1
+        arr.append(input[i])
+        if cut == count {
+            cut = 0
+            if temp == arr {
+                comcount += 1
+            } else {
+                if comcount != 1 {
+                    output+="\(comcount)"
                 }
+                for i in temp {
+                    output += i
+                }
+                temp = arr
+                comcount = 0
             }
         }
-        return
-    }
-    
-    var xs = xstart
-    var xe = xs + sum
-    
-    for i in 0..<3 {
-        var ys = ystart
-        var ye = ys + sum
-        for j in 0..<3 {
-            if i == 1 && j == 1 {
-                ys = ye + 1
-                ye = ys + sum
-                continue
-            }
-            recur(xstart: xs, ystart: ys, xend: xe, yend: ye)
-            ys = ye + 1
-            ye = ys + sum
+        
+        for i in arr {
+            output += i
         }
-        xs = xe + 1
-        xe = xs + sum
     }
+    
+    return output.count
 }
 
 
-recur(xstart: 0, ystart: 0, xend: n-1  , yend: n-1)
-
-for i in 0..<n {
-    for j in 0..<n {
-        print("\(arr[i][j])", terminator: "")
-    }
-    print("")
+for i in 1...input.count {
+    let result = compression(count: i)
+    
+    minvalue = min( minvalue, result)
 }
 
+
+print(minvalue)
