93d447e74422faa9864fe5ab6b1488b32c0fc37a
Wonder Park <pkkyung26@gmail.com>
2023-03-15 23:59:01 +0900

    solve: 14916

---
+++ Algorithm/BOJ/Greedy/14916_거스름돈.swift
@@ -0,0 +1,33 @@
+//
+//  14916_거스름돈.swift
+//  Algorithm
+//
+//  Created by 박희경 on 2023/03/15.
+//
+
+//import Foundation
+//
+//var n = Int(readLine()!)!
+//var count = 0
+//
+//// 가장 큰 이득 = 거스름돈이 5로 나누어 떨어질때
+//// 그 다음 = 거스름돈이 5로 나누어 떨어지지 않으면 5로 떨어질때까지 -2
+//// 거슬러줄수없으면 ? == while문을 빠져나갈때 n!=0 (n<0)
+//
+//while n>0 {
+//    if n % 5 == 0 {
+//        count += n / 5
+//        break
+//    } else {
+//        n -= 2
+//        count += 1
+//    }
+//}
+//
+//if n<0 {
+//    print(-1)
+//} else {
+//    print(count)
+//}
+//
+//
