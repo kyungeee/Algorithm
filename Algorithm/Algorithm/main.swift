a5e096910ef963ae98fa501c32293bb0fbb20e87
Wonder Park <pkkyung26@gmail.com>
2023-05-06 15:42:23 +0900

    solve: 11726

--- Algorithm/Algorithm/main.swift
+++ Algorithm/Algorithm/main.swift
@@ -7,30 +7,25 @@
 
 import Foundation
 
-let t = Int(readLine()!)!
+let n = Int(readLine()!)!
 
-var dp = [Int](repeating: 0, count: 11)
+var dp = [Int](repeating: 0, count: n+1)
 
-dp[1] = 1
-dp[2] = 2
-dp[3] = 4
 
-// ex) n = 5
-// dp[4] 4를 만들수 있는 모든 경우의 수에 +1 한 것과 같음
-// dp[3] 3를 만들수 있는 모든 경우의 수에 +2 한 것과 같음
-// dp[2] 2를 만들수 있는 모든 경우의 수에 +3 한 것과 같음
-for _ in 0..<t {
-    let n = Int(readLine()!)!
+if n == 1 {
+    print("1")
+} else if n == 2 {
+    print("2")
+} else {
+    dp[1] = 1
+    dp[2] = 2
     
-    for i in 4...n {
-        dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+    for i in 3...n {
+        dp[i] = (dp[i-1] + dp[i-2]) % 10007
     }
     
     print(dp[n])
 }
-
-
-
 
 
 
