b874e3e4e8cdaf2db2f35d23f07c7d3e31c6e0c3
Wonder Park <pkkyung26@gmail.com>
2023-05-06 23:57:19 +0900

    solve: 17626

--- Algorithm/Algorithm/main.swift
+++ Algorithm/Algorithm/main.swift
@@ -7,25 +7,33 @@
 
 import Foundation
 
+// 런타임 에러뜸 왜인지는 아직 모르겠음...
+// dp[1] = 1 로 초기화 시켜주고, 밑에 for 문 바깥에 n>1 일때 라는 조건을 하나 달아주니 맞았음.
+
 let n = Int(readLine()!)!
 
-var dp = [Int](repeating: 0, count: n+1)
+//  dp table
+var dp = Array(repeating: 0, count: n+1)
 
+dp[1] = 1
 
-if n == 1 {
-    print("1")
-} else if n == 2 {
-    print("2")
-} else {
-    dp[1] = 1
-    dp[2] = 2
-    
-    for i in 3...n {
-        dp[i] = (dp[i-1] + dp[i-2]) % 10007
+// 처음 N과 가장 가까운 제곱수만 생각하면 되는 것이 아니라 모든 제곱수에 대한 경우의 수 도 다 찾아봐야함.
+if n > 1 {
+    for i in 2...n {
+        dp[i] = dp[1] + dp[i-1]
+        var j = 2
+        while j*j <= i {
+            dp[i] = min(dp[i], 1 + dp[i - j * j])
+            j += 1
+        }
     }
-    
-    print(dp[n])
 }
+
+print(dp[n])
+
+
+
+
 
 
 
--- Algorithm/BOJ/DP/17626_FourSquares.swift
+++ Algorithm/BOJ/DP/17626_FourSquares.swift
@@ -9,10 +9,11 @@ import Foundation
 
 
 // 런타임 에러뜸 왜인지는 아직 모르겠음...
+// dp[1] = 1 로 초기화 시켜주고, 밑에 for 문 바깥에 n>1 일때 라는 조건을 하나 달아주니 맞았음.
 
 //let n = Int(readLine()!)!
-//
-////  dp table
+
+//  dp table
 //var dp = Array(repeating: 0, count: 50001)
 
 
@@ -46,19 +47,18 @@ import Foundation
 //
 
 // 처음 N과 가장 가까운 제곱수만 생각하면 되는 것이 아니라 모든 제곱수에 대한 경우의 수 도 다 찾아봐야함.
-//for i in 2...n {
-//    dp[i] = dp[1] + dp[i-1]
-//    var j = 2
-//    while j*j <= i {
-//        if j*j == i {
-//            dp[i] = 1
-//            break
+// 처음 N과 가장 가까운 제곱수만 생각하면 되는 것이 아니라 모든 제곱수에 대한 경우의 수 도 다 찾아봐야함.
+//if n > 1 {
+//    for i in 2...n {
+//        dp[i] = dp[1] + dp[i-1]
+//        var j = 2
+//        while j*j <= i {
+//            dp[i] = min(dp[i], 1 + dp[i - j * j])
+//            j += 1
 //        }
-//        dp[i] = min(dp[i], 1 + dp[i - j * j])
-//        j+=1
 //    }
 //}
-//
+
 //print(dp[n])
 
 
