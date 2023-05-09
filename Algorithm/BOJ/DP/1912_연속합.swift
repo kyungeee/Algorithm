3c408e1c084d44c5d630a364ffaaee7cc426c48d
Wonder Park <pkkyung26@gmail.com>
2023-05-09 22:14:57 +0900

    solve: 1912

--- Algorithm/BOJ/DP/1912_연속합.swift
+++ Algorithm/BOJ/DP/1912_연속합.swift
@@ -5,4 +5,18 @@
 //  Created by 박희경 on 2023/05/09.
 //
 
-import Foundation
+//import Foundation
+//
+//let n = Int(readLine()!)!
+//
+//var cache = readLine()!.split(separator: " ").map { Int(String($0))!
+//}
+//
+//for i in 1..<n {
+//    if cache[i] < cache[i-1] + cache[i] {
+//        cache[i] = cache[i] + cache[i-1]
+//    }
+//}
+//
+//var maxSequenceNum = cache.reduce(Int.min) { max($0, $1) }
+//print(maxSequenceNum)
