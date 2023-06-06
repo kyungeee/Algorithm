8e7f0205bececed769e4c0e0ba7a30c6b16dc3df
Wonder Park <pkkyung26@gmail.com>
2023-06-06 16:08:21 +0900

    solve: 15651

---
+++ Algorithm/BOJ/Backtracking/15651_N과M(3).swift
@@ -0,0 +1,35 @@
+//
+//  15651_N과M(3).swift
+//  Algorithm
+//
+//  Created by 박희경 on 2023/06/02.
+//
+
+import Foundation
+
+//let input: [Int] = readLine()!.split(separator: " ").map{Int(String($0))!}
+//let n = input[0], m = input[1]
+//
+//var output: [Int] = []
+//var str = ""
+//
+//func dfs(depth: Int) {
+//    if depth == m {
+//        str += output.map({String ($0)}).joined(separator: " ")
+//        str += "\n"
+//        return
+//    }
+//
+//    for i in 1...n {
+//            output.append(i)
+//            dfs(depth: depth+1)
+//            output.removeLast()
+//        }
+//    }
+//
+//dfs(depth: 0)
+//print(str)
+//
+//
+//
+//
